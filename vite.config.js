3209012b4fc7f6a709a483a137634b10b7e15ed5
JulioSosaAng <julessosa1@gmail.com>
2025-09-16 12:52:45 -0300

    Second Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/mi-portfolio/", // ¡Asegúrate de que el nombre coincida exactamente!
   plugins: [react()],
-})
+});
